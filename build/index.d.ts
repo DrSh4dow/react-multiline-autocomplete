36d6dde7ebbb391e9f2fe03b80b1b4a083401805
DrSh4dow <daniel.morettiv@gmail.com>
2019-11-06 15:33:56 -0300

    added the base of the component

---
+++ build/index.d.ts
@@ -0,0 +1,7 @@
+import * as React from "react";
+export declare type Props = {
+    text: string;
+};
+export default class ExampleComponent extends React.Component<Props> {
+    render(): JSX.Element;
+}
